6027a4577d830abb9032ad9771a0676cdb9ee19f
Sergey Gusakov <firo077@yandex.ru>
2021-02-17 21:40:09 +0400

    Домашнее задание №3

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -1,55 +1,85 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HomeWork {
+
+    public static Random rand = new Random();
+
+
     public static void main(String[] args) {
-        byte a = 2; short b = 15; int c = 1000000; long d = 2000000000000l; float e = 1.3f;
-        double f = 2.6; char ch = '*'; boolean bool = true;
-        System.out.println(calculate(1.2f, 3.1f,2.2f,1.4f));
-        System.out.println(task10and20(10,8));
-        isPositiveOrNegative(0);
-        System.out.println(isNegative(-4));
-        greetings();
-        year (2021);
-    }
-    public static float calculate (float a, float b, float c, float d) {
-        float result = a * (b + (c / d));
-        return result;
-    }
-    public static boolean task10and20 (int a, int b) {
-        int result = a + b;
-        if (result <= 10 && result >= 20) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static void isPositiveOrNegative (int a) {
-        if (a >= 0){
-            System.out.println(a + " Положительное число");
-        } else {
-            System.out.println(a + " Отрицательное число");
-        }
-    }
-    public static boolean isNegative (int a) {
-        if (a < 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static void greetings () {
-        System.out.println("Hello, Сергей");
+        guessNumber();
+
+        guessFruit();
     }
 
-    public static void year (int a) {
-        if (a % 400 == 0) {
-            System.out.println("Високосный");
-        } else if (a % 100 == 0){
-            System.out.println("Не високосный");
-        } else if (a % 4 == 0){
-            System.out.println("Високосный");
-        } else {
-            System.out.println("Не високосный");
-        }
+    public static void guessNumber(){
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            int number = rand.nextInt(10);
+            System.out.println("Угадайте число от 0 до 9");
+            for (int i = 3; i > 0; i--) {
+                System.out.println("Введите число");
+                int inputNumber = readIntNumberFromConsole();
+                if (inputNumber == number) {
+                    System.out.println("Поздравляю! Вы угадали!!!");
+                    break;
+                } else if (inputNumber < number) {
+                    System.out.println("Загаданное число больше");
+                } else if (inputNumber > number) {
+                    System.out.println("Загаданное число меньше");
+                }
+                if (i - 1 > 0) {
+                    System.out.println(" у вас осталось " + (i - 1) + " попыток");
+                } else System.out.println("Попытки закончились");
+            }
+            System.out.println("Повторить игру еще раз? 1 - да; 0 - нет");
+        } while (sc.nextInt() == 1);
     }
 
+    static int readIntNumberFromConsole() {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            if (sc.hasNextInt()) {
+                return sc.nextInt();
+            }
+            sc.nextLine();
+            System.out.println("Введите ЦЕЛОЕ ЧИСЛО!");
+        } while (true);
+    }
+
+    public static void guessFruit (){
+        String [] fruits = {"apple", "orange","lemon","banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "peper", "pineapple", "pumpkin", "potato"};
+
+
+        int wordIndex = rand.nextInt(fruits.length -1);
+        String word = fruits[wordIndex];
+        int rangeWord = word.length();
+        System.out.println("Я загадал слово, попробуй отгадать его!");
+//        System.out.println(word);
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Введите ответ:");
+            String answer = sc.nextLine();
+            if (answer.equals(word)){
+                System.out.println("Поздравляю, вы угадали!!");
+                break;
+            }
+            char[] coment = new char [15];
+            for (int i = 0; i < coment.length; i++) {
+                coment[i] = '#';
+            }
+            char [] charsAnswer = answer.toCharArray();
+            for (int i = 0; i < rangeWord; i++){
+                if (i >= charsAnswer.length) break;
+                if (word.charAt(i) == charsAnswer[i]) coment[i] = word.charAt(i);
+            }
+            System.out.println(coment);
+        }while (true);
+    }
 }
-
